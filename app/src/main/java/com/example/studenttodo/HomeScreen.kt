b3b6124e228b5df0e1609fc1d78d6d2bc9aefbb3
Max Wilkinson <mwilkinson4@sheffield.ac.uk>
2023-11-08 15:57:24 +0000

    Added todos for home page

--- app/src/main/java/com/example/studenttodo/HomeScreen.kt
+++ app/src/main/java/com/example/studenttodo/HomeScreen.kt
@@ -7,7 +7,12 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen (name: String, modifier: Modifier = Modifier) {
-        Text(
+/* TODO displays a list of tasks
+ * TODO Colour based on priority ability to mark as completed
+ * TODO Click to open page to update/view
+ * TODO ordered by date/priority
+ */
+    Text(
             text = "Hello $name!",
             modifier = modifier
         )
