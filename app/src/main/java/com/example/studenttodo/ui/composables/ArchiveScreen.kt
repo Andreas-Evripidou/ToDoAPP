b1778f93c7cda301ebf608b2d149e5c85cc82855
Andreas-Evripidou <eandreas1@sheffield.ac.uk>
2023-11-13 13:47:26 +0000

    Feat(Archive Screen): Remove unwanted imports

--- app/src/main/java/com/example/studenttodo/ui/composables/ArchiveScreen.kt
+++ app/src/main/java/com/example/studenttodo/ui/composables/ArchiveScreen.kt
@@ -1,6 +1,5 @@
 package com.example.studenttodo.ui.composables
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -24,12 +22,8 @@ import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -38,68 +32,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.studenttodo.entities.ToDoEntity
 import com.example.studenttodo.viewmodels.ArchiveViewModel
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.forEach
-import kotlinx.coroutines.launch
-
-@Composable
-fun ArchiveScreenTemp() {
-    Column {
-
-        var bold by remember { mutableStateOf(false) }
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text("Bold")
-            Spacer(modifier = Modifier.size(8.dp))
-            Switch(checked = bold, onCheckedChange = { bold = it })
-        }
-        Spacer(modifier = Modifier.size(38.dp))
-        val choices = listOf("small", "medium", "large", "extra large")
-        val (size, onSelected) = remember { mutableStateOf(choices[1]) }
-        Text(text = "Size")
-        choices.forEach { text ->
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                RadioButton(selected = (text == size), onClick = { onSelected(text) })
-                Text(text)
-            }
-        }
-        Spacer(modifier = Modifier.size(38.dp))
-
-        val weekdays = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
-        var selectedDays by remember { mutableStateOf(BooleanArray(weekdays.size) { false }) }
-        weekdays.forEachIndexed { index, weekday ->
-            var thisDay by remember { mutableStateOf(selectedDays[index]) }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 10.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(weekday)
-                Checkbox(
-                    checked = thisDay,
-                    onCheckedChange = {
-                        thisDay = !thisDay
-                        selectedDays[index] = thisDay
-                    })
-            }
-
-        }
-        Button({ Log.i("LogLog", selectedDays.contentToString()) }) {
-            Text(text = "Log")
-        }
-
-    }
-}
 
 @Composable
 fun ShowToDo(
