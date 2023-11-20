827d26def3ac1de9edb249dbcab1773dd3e2f5f9
Louis <ljawillison1@sheffield.ac.uk>
2023-11-20 20:54:01 +0000

    Added a "date picker" style of input to the Create page Improved input validation Changed text box style to outlined as it looks more slick

--- app/src/main/java/com/example/studenttodo/ui/composables/CreateScreen.kt
+++ app/src/main/java/com/example/studenttodo/ui/composables/CreateScreen.kt
@@ -3,14 +3,19 @@ package com.example.studenttodo.ui.composables
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -22,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.studenttodo.entities.ToDoEntity
@@ -30,6 +36,7 @@ import com.example.studenttodo.viewmodels.HomeViewModel
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -46,13 +53,13 @@ fun CreateScreen() {
         var taskdescription by remember { mutableStateOf("") }
         var locationradius by remember { mutableStateOf("") }
         Text("Task Name")
-        TextField(
+        OutlinedTextField(
             value = taskname,
             onValueChange = { taskname = it },
             label = { Text("task title") })
 
         Text("Task Description")
-        TextField(
+        OutlinedTextField(
             value = taskdescription,
             onValueChange = { taskdescription = it },
             label = { Text("task description") })
@@ -78,13 +85,66 @@ fun CreateScreen() {
         var time by remember { mutableStateOf("") }
 
         Text("Reminder Date")
-        TextField(
-            value = date,
-            onValueChange = { date = it },
-            label = { Text("in format DD/MM/YYYY") })
+        var day by remember { mutableStateOf("") }
+        var month by remember { mutableStateOf("") }
+        var year by remember { mutableStateOf("") }
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.width(70.dp),
+                value = day,
+                onValueChange = { textInput ->
+
+                    day = textInput.take(2)
+
+                },
+                label = { Text("DD") },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+
+            Text("/")
+
+
+            OutlinedTextField(
+                modifier = Modifier.width(70.dp),
+                value = month,
+                onValueChange = { textInput ->
+
+                    month =textInput.take(2)
+
+                },
+                label = { Text("MM") },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+
+            Text("/")
+
+
+            OutlinedTextField(
+                modifier = Modifier.width(80.dp),
+                value = year,
+                onValueChange = { textInput ->
+                    year =textInput.take(4)
+
+                },
+                label = { Text("YYYY") },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+        }
+        date = day.plus("/").plus(month).plus("/").plus(year)
+        Spacer(modifier = Modifier.height(16.dp))
 
         Text("Reminder Time")
-        TextField(
+        OutlinedTextField(
             value = time,
             onValueChange = { time = it },
             label = { Text("in format HH:MM") })
@@ -96,22 +156,32 @@ fun CreateScreen() {
         Text("Pick Location (Not taught yet")
 
         Text("Location Radius")
-        TextField(
+        OutlinedTextField(
             value = locationradius,
             onValueChange = { locationradius = it },
             label = { Text("location radius") })
 
         var showError by remember { mutableStateOf(false) }
+        var showDateError by remember { mutableStateOf(false) }
         SubmitButton {
             var lDate = LocalDate.now()
             var lTime = LocalTime.now()
 
 
             if (date.isNotEmpty() && time.isNotEmpty() && taskname.isNotEmpty() && taskdescription.isNotEmpty() && locationradius.isNotEmpty()) {
-                val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-                val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
 
-                lDate = LocalDate.parse(date, dateFormatter)
+                val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+                val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+                try {
+                    lDate = LocalDate.parse(date, dateFormatter)
+
+                } catch (e: DateTimeParseException) {
+                    // Error handling: Print an error message or take appropriate action
+
+                    showDateError = true
+                }
+
                 lTime = LocalTime.parse(time, timeFormatter)
 
                 val todo = ToDoEntity(
@@ -139,13 +209,25 @@ fun CreateScreen() {
         if (showError) {
             displayError()
         }
+        if(showDateError){
+            displayDateTimeError()
+        }
     }
+
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerDemo() {
 
 }
 
     @Composable
     fun displayError(){
         Text("Please fill in all fields", color = MaterialTheme.colorScheme.error)
+    }
+    @Composable
+    fun displayDateTimeError(){
+        Text("Please enter a valid date and time in the correct format", color = MaterialTheme.colorScheme.error)
     }
 
     @Composable
