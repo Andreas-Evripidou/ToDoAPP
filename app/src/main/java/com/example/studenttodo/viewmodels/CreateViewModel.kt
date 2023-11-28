d4d661020d6704643718b05e2d600ada169f1f00
sizyk <stylianos.kz@hotmail.com>
2023-11-28 13:58:25 +0000

    Dialog to Create Modules from the create page

--- app/src/main/java/com/example/studenttodo/viewmodels/CreateViewModel.kt
+++ app/src/main/java/com/example/studenttodo/viewmodels/CreateViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.studenttodo.data.ToDoDatabase
+import com.example.studenttodo.entities.ModuleEntity
 import com.example.studenttodo.entities.ToDoEntity
 import kotlinx.coroutines.launch
 
@@ -18,5 +19,9 @@ class CreateViewModel (app:Application): AndroidViewModel(app){
 
     fun createToDo (toDo: ToDoEntity) = viewModelScope.launch {
         dao.insert(toDo)
+    }
+
+    fun createModule(module: ModuleEntity) = viewModelScope.launch {
+        moduledao.updateOrInsert(module)
     }
 }
